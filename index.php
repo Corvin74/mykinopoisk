69f1a70bc57370920c5ded705a738e9c5bb0efb2
Konstantin N. Moiseev <moiseev-kn@mail.ru>
2020-12-11 14:43:47 +0300

    Add bootstrap

--- index.php
+++ index.php
@@ -28,15 +28,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Мой кинопоиск</title>
     <link rel="stylesheet" href="../css/reset.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,500;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <header class="header">
+    <header class="container header">
         <h1>Мой кинопоиск</h1>
     </header>
-    <div class="section">
+    <div class="container section">
         <div class="toprow">
             <div class="cell htitle">Название</div>
             <div class="cell hpremiere">Примьера</div>
